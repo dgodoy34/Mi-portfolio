11613913095391f98fa00230fcacd77677777bd0
Diego Godoy <dgodoy34@gmail.com>
2026-02-04 19:04:39 -0300

    Configura dominios externos para next/image

--- next.config.js
+++ next.config.js
@@ -6,3 +6,19 @@ module.exports = {
   ],
   reactStrictMode: true,
 };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mdtstreet.mdtsublimados.com.ar',
+        port: '',
+        pathname: '/img/**',  // opcional, pero recomendado para limitar a /img/
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
